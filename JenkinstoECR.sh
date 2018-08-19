e483faeadae42a50db400693bf53d8d1f4d751b3
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-08-19 21:12:51 +0530

    Update JenkinstoECR.sh

--- JenkinstoECR.sh
+++ JenkinstoECR.sh
@@ -1,4 +1,4 @@
-echo $USER
+echo $USE
 docker build -t test .
 docker tag test:latest 466749146115.dkr.ecr.ap-south-1.amazonaws.com/test:0.0.1
 eval $(aws ecr get-login --region ap-south-1 --no-include-email | sed "s|https://||")
