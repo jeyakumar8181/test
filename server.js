8317129127417fab1f9e72e7febbd356b0b36f88
jeyakumar8181 <37777450+jeyakumar8181@users.noreply.github.com>
2018-08-19 21:11:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function(req, res){
 
 res.writeHead(200, {'content-type':'text/plain'});
-res.end('Welcome jaikumar');
+res.end('Welcome');
 }).listen(8080);
 
 console.log('Server is running on a port over 8080 !!');
